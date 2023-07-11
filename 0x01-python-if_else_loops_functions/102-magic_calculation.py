fde2232bb339e7158e1f9138332578af56d3929e
Purengugi <purity2004ngugi@gmail.com>
2023-07-11 09:34:35 -0700

    update

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def magic_calculation(a, b, c):
     if a < b:
-    return(c)
+       return(c)
     elif c > b:
-    return(a + b)
+       return(a + b)
     else:
-    return(a * b - c)
+       return(a * b - c)
