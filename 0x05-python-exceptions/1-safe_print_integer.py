6e6d8bcd86b32bd1dd32f6b0fbc7518065703030
Purengugi <purity2004ngugi@gmail.com>
2023-07-31 16:18:21 +0000

    update

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value))
-    except(ValueError, TypeError):
-        return False
-    else:
+        print('{:d}'.format(value))
         return True
+    except (TypeError, ValueError):
+        return False
