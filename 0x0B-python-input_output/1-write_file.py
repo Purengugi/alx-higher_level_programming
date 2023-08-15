40497ca31c69f5b07bd9a182e5dbe1749b9f97d7
Purengugi <purity2004ngugi@gmail.com>
2023-08-15 17:13:32 +0000

    update

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-"""Defines a file-writing function."""
+"""
+Module calls write_file function to write to a
+text file.
+"""
 
 
 def write_file(filename="", text=""):
-    """Write a string to a UTF8 text file.
-    Args:
-        filename (str): The name of the file to write.
-        text (str): The text to write to the file.
-    Returns:
-        The number of characters written.
     """
-    with open(filename, "w", encoding="utf-8") as f:
-        return f.write(text) 
+    Writes a string to a text file (UTF8)
+    and returns the number of characters written:
+    """
+    with open(filename, 'w') as f:
+        f.write(text)
+        num_chars = len(text)
+        return num_chars
